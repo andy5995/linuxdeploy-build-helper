4c6b1a2080079c576a911778117f9615bd7daa43
andy5995 <arch_stanton5995@proton.me>
2024-11-23 21:50:22 -0600

    Improve (and fix) entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+set -e
+
 echo ""
-echo "linuxdeploy Build Helper Container v2.0.0"
+echo "linuxdeploy Build Helper Container v2.0.1"
 echo "https://github.com/andy5995/linuxdeploy-build-helper-container"
 echo ""
 
 OLDPWD=$PWD
 
-if [ -z "HOSTUID" ]; then
+if [ -z "$HOSTUID" ]; then
   echo "HOSTUID is not set."
+  exit 1
+fi
+
+if [ -z "$HOSTGID" ]; then
+  echo "HOSTGID is not set."
   exit 1
 fi
 
@@ -17,5 +24,9 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-usermod -u $HOSTUID builder
-su builder -c "PATH=/home/builder/.local/bin:$PATH && cd $OLDPWD && $1"
+usermod -u "$HOSTUID" builder
+chown -R "$HOSTUID:$HOSTGID" /home/builder
+
+# The docs state to use '-w /workdir when running the container, but switching
+# to builder here will change the directory. Using cd to change back...
+su builder -l -c "cd $OLDPWD && $1"
